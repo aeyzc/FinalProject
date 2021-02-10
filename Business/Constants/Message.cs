5c7ccb257a745a9667269c9e5e6cda8d3c1b28cd
aeyzc <aeyazc@gmail.com>
2021-02-10 23:58:32 +0300

    10 subat

---
+++ Business/Constants/Message.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Message
+    {
+        public static string ProductAdded = "Ürün Eklendi!";
+        public static string ProductNameInvalid = "Ürün ismi geçersiz!";
+        internal static string MaintenanceTime="Saat 22'den sonra işlem yapılamaz";
+        internal static string ProductsListed="Ürünler listelendi";
+    }
+}
